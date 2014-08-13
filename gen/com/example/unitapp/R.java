39bb08d8e38adf8d5671afcb1a0c7f3bd7a7233f
sagargarg <sagargarg@gmail.com>
2014-08-13 10:14:28 -0400

    Added a number box for the user to type

--- gen/com/example/unitapp/R.java
+++ gen/com/example/unitapp/R.java
@@ -25,7 +25,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
+        public static final int action_settings=0x7f080007;
+        public static final int editText1=0x7f080006;
         public static final int ftpsec=0x7f080004;
         public static final int mph=0x7f080003;
         public static final int number=0x7f080002;
